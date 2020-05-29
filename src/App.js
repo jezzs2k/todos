e73438f6492a8873c8763a979ab304ec8fdb4ed7
HIEU VU THANH <jezzs2k@gmail.com>
2020-05-29 10:20:01 +0700

    repo todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [
+    'goToBed',
+    'have breakfast',
+    'goToSchool',
+    'learn codersX',
+    'learn vocabulary',
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ul>
+        {todos &&
+          todos.map((item) => {
+            return <li>{item}</li>;
+          })}
+      </ul>
     </div>
   );
 }
